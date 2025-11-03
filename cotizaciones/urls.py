145cdee22eb137fe5b870232ea7cae7c28b78e60
Cookeing <jp.mal.hue@gmail.com>
2025-11-03 12:33:45 -0300

    HU08, HU09 (en proceso)

--- cotizaciones/urls.py
+++ cotizaciones/urls.py
@@ -5,7 +5,12 @@ app_name = "cotizaciones"
 
 urlpatterns = [ 
     path("registrar/", v.RegistrarCotizacion, name="registrar_cotizacion"),
+    path('editar/<int:pk>/', v.EditarCotizacion, name='editar_cotizacion'),
+    path('eliminar/<int:pk>/', v.EliminarCotizacion, name='eliminar_cotizacion'),
+    path('duplicar/<int:pk>/', v.DuplicarCotizacion, name='duplicar_cotizacion'),
     path("historial/", v.historial_cotizaciones, name="listar_cotizaciones"),
     path("cliente/<int:cliente_id>/data/", v.get_cliente_data, name="cliente_data"),
+    path('pdf/<int:pk>/', v.VerPDF, name='ver_pdf'),
+    path('listar/', v.historial_cotizaciones, name='listar_cotizaciones'),
 ]
 
